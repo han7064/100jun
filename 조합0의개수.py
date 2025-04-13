55f37a9c34332e30806e7848ef43b20d67927c88
han7064 <130061704+han7064@users.noreply.github.com>
2025-04-13 12:03:04 +0000

    25.04.13

--- 조합0의개수.py
+++ 조합0의개수.py
@@ -4,10 +4,9 @@ n,m = map(int, sys.stdin.readline().split())
 
 def cnt_k(n, k):
     res = 0
-    cnt = k
     while n>1:
-        res += n//cnt
-        n = n//cnt
+        res += n//k
+        n = n//k
     return res
 
 two = cnt_k(n,2) - cnt_k(m,2) - cnt_k(n-m,2)
