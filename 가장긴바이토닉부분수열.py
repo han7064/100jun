f4162eacc4f4c6d0f0d7270e20eb0078cf032aa8
han7064 <130061704+han7064@users.noreply.github.com>
2025-04-18 14:00:14 +0000

    25.4.18

--- 가장긴바이토닉부분수열.py
+++ 가장긴바이토닉부분수열.py
@@ -1,2 +1,17 @@
 n = int(input())
 a = list(map(int, input().split()))
+reverseA = a[::-1]
+dp_up = [1]*n
+dp_down = [1]*n
+for i in range(n):
+    for j in range(i):
+        if a[i]>a[j]:
+            dp_up[i] = max(dp_up[i],dp_up[j]+1)
+        
+        if reverseA[i]>reverseA[j]:
+            dp_down[i] = max(dp_down[i],dp_down[j]+1)
+dp_down = dp_down[::-1]
+res = []
+for i in range(n):
+    res.append(dp_up[i]+dp_down[i]-1)
+print(max(res))
